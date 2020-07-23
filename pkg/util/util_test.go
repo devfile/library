ab425fc8fb698ae69dd92eb742d91ebfdc69d212
ranakan19 <46766610+ranakan19@users.noreply.github.com>
2020-07-23 14:22:24 -0400

    Update pkg/util/util_test.go
    Co-authored-by: Serhii Leshchenko <sleshche@redhat.com>

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -1969,7 +1969,7 @@ func TestDownloadInMemory(t *testing.T) {
 	}{
 		{
 			name: "Case 1: valid URL",
-			url:  "https://github.com/devfile/devfile-parser/blob/master/tests/examples/source/devfiles/nodejs/devfile.yaml",
+			url:  "https://github.com/devfile/devfile-parser/blob/master/devfile.yaml",
 			want: true,
 		},
 		{
