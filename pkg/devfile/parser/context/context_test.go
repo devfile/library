058724c7e22318ace4725545346f6f82cc204e63
William Tam <email.wtam@gmail.com>
2020-09-07 09:40:31 -0400

    Refactored TestPopulateFromBytes to use table drive tests

--- pkg/devfile/parser/context/context_test.go
+++ pkg/devfile/parser/context/context_test.go
@@ -7,62 +7,51 @@ import (
 )
 
 func TestPopulateFromBytes(t *testing.T) {
-
-	const (
-		InvalidURL = "blah"
-	)
-
-	t.Run("valid data passed", func(t *testing.T) {
-
-		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			_, err := w.Write(validJsonRawContent200())
-			if err != nil {
-				t.Error(err)
+	tests := []struct {
+		name        string
+		dataFunc    func() []byte
+		expectError bool
+	}{
+		{
+			name:        "valid data passed",
+			dataFunc:    validJsonRawContent200,
+			expectError: false,
+		},
+		{
+			name:        "invalid data passed",
+			dataFunc:    invalidJsonRawContent200,
+			expectError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_, err := w.Write(tt.dataFunc())
+				if err != nil {
+					t.Error(err)
+				}
+			}))
+			var (
+				d = DevfileCtx{
+					url: testServer.URL,
+				}
+			)
+			defer testServer.Close()
+			err := d.PopulateFromURL()
+			if tt.expectError && err == nil {
+				t.Errorf("expected error, didn't get one")
+			} else if !tt.expectError && err != nil {
+				t.Errorf("unexpected error '%v'", err)
 			}
-		}))
+		})
+	}
+}
 
+func TestPopulateFromInvalidURL(t *testing.T) {
+	t.Run("Populate from invalid URL", func(t *testing.T) {
 		var (
 			d = DevfileCtx{
-				url: testServer.URL,
-			}
-		)
-		defer testServer.Close()
-
-		err := d.PopulateFromURL()
-
-		if err != nil {
-			t.Errorf("unexpected error '%v'", err)
-		}
-	})
-
-	t.Run("invalid data passed", func(t *testing.T) {
-
-		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			_, err := w.Write([]byte(InvalidDevfileContent))
-			if err != nil {
-				t.Error(err)
-			}
-		}))
-
-		var (
-			d = DevfileCtx{
-				url: testServer.URL,
-			}
-		)
-		defer testServer.Close()
-
-		err := d.PopulateFromURL()
-
-		if err == nil {
-			t.Errorf("expected error, didn't get one ")
-		}
-	})
-
-	t.Run("invalid filepath", func(t *testing.T) {
-
-		var (
-			d = DevfileCtx{
-				url: InvalidURL,
+				url: "blah",
 			}
 		)
 
@@ -72,4 +61,8 @@ func TestPopulateFromBytes(t *testing.T) {
 			t.Errorf("expected an error, didn't get one")
 		}
 	})
+}
+
+func invalidJsonRawContent200() []byte {
+	return []byte(InvalidDevfileContent)
 }
