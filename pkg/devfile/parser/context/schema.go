4584c825b8889c421c1e8e7f4267eaa5140a9935
Stephanie <yangcao@redhat.com>
2020-10-01 14:39:38 -0400

    get the schema from URL
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- pkg/devfile/parser/context/schema.go
+++ pkg/devfile/parser/context/schema.go
@@ -23,9 +23,9 @@ func (d *DevfileCtx) SetDevfileJSONSchema() error {
 
 // ValidateDevfileSchema validate JSON schema of the provided devfile
 func (d *DevfileCtx) ValidateDevfileSchema() error {
-
 	var (
-		schemaLoader   = gojsonschema.NewStringLoader(d.jsonSchema)
+		// schemaLoader   = gojsonschema.NewStringLoader(d.jsonSchema)
+		schemaLoader   = gojsonschema.NewReferenceLoader("https://raw.githubusercontent.com/devfile/api/master/schemas/latest/devfile.json")
 		documentLoader = gojsonschema.NewStringLoader(string(d.rawContent))
 	)
 
