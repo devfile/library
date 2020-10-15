cc6712b4e628a2ebf06303f3e911286f428457a1
Maysun J Faisal <maysunaneek@gmail.com>
2020-10-15 14:43:24 -0400

    Remove outdated validation code
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- pkg/devfile/validate/validate.go
+++ pkg/devfile/validate/validate.go
@@ -1,37 +1,14 @@
 package validate
 
-// import (
-// 	"fmt"
+import (
+	"k8s.io/klog"
+)
 
-// 	"k8s.io/klog"
+// ValidateDevfileData validates whether sections of devfile are odo compatible
+func ValidateDevfileData(data interface{}) error {
 
-// 	v1 "github.com/devfile/api/pkg/apis/workspaces/v1alpha2"
-// 	v2 "github.com/devfile/parser/pkg/devfile/parser/data/v2"
-// )
+	// Skipped
+	klog.V(4).Info("No validation present. Skipped for the moment.")
+	return nil
 
-// // ValidateDevfileData validates whether sections of devfile are odo compatible
-// func ValidateDevfileData(data interface{}) error {
-// 	var components []v1.Component
-
-// 	switch d := data.(type) {
-// 	case *v2.DevfileV2:
-// 		components = d.GetComponents()
-
-// 		// Validate Events
-// 		if err := validateEvents(d); err != nil {
-// 			return err
-// 		}
-// 	default:
-// 		return fmt.Errorf("unknown devfile type %T", d)
-// 	}
-
-// 	// Validate Components
-// 	if err := validateComponents(components); err != nil {
-// 		return err
-// 	}
-
-// 	// Successful
-// 	klog.V(4).Info("Successfully validated devfile sections")
-// 	return nil
-
-// }
+}
