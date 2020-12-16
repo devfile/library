cc1911fda37d7a665824bed6c7fef1ff1705d8ad
Martin Mulholland <mmulholl@us.ibm.com>
2020-12-16 14:26:32 -0500

    trial and error

--- tests/src/tests/parser_verify_test.go
+++ tests/src/tests/parser_verify_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestExecCommand(t *testing.T) {
+func Test_execCommand(t *testing.T) {
 	if GetRandomNumber(10) > 10 {
 		t.Error("Randon mumber was bigger than 10")
 	}
