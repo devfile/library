3a97b130f136cd6d0e746110652e418e8f043740
Martin Mulholland <mmulholl@us.ibm.com>
2020-12-16 14:22:50 -0500

    trial and error

--- tests/src/tests/parser_verify_test.go
+++ tests/src/tests/parser_verify_test.go
@@ -2,18 +2,10 @@ package tests
 
 import (
 	"testing"
-
-	schema "github.com/devfile/api/pkg/apis/workspaces/v1alpha2"
 )
 
 func TestExecCommand(t *testing.T) {
-	testContent := TestContent{}
-	testContent.CommandTypes = []schema.CommandType{schema.ExecCommandType}
-	testContent.CreateWithParser = false
-	testContent.EditContent = false
-	testContent.FileName = GetDevFileName()
-	err := RunTest(testContent)
-	if err != nil {
-		t.Fatalf("ERROR : %v", err)
+	if GetRandomNumber(10) > 10 {
+		t.Error("Randon mumber was bigger than 10")
 	}
 }
