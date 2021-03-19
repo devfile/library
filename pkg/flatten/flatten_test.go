d33c7285a6d589790df05d24abf7fa221be37d50
Angel Misevski <amisevsk@redhat.com>
2021-03-19 19:06:39 -0400

    Add test cases to cover cases where incomplete tooling is provided
    Adds test cases to cover:
    - Using resolver when HttpClient or K8sClient are nil
    - Resolving Kubernetes resources when no namespace can be determined
    - Resolving resources by ID when no registryURL can be determined
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/flatten/flatten_test.go
+++ pkg/flatten/flatten_test.go
@@ -33,8 +33,9 @@ func TestResolveDevWorkspaceKubernetesReference(t *testing.T) {
 				Errors:                tt.Input.Errors,
 			}
 			testResolverTools := ResolverTools{
-				Context:   context.Background(),
-				K8sClient: testClient,
+				Context:          context.Background(),
+				K8sClient:        testClient,
+				DefaultNamespace: "default-namespace",
 			}
 			outputWorkspace, err := ResolveDevWorkspace(&tt.Input.Workspace, testResolverTools)
 			if tt.Output.ErrRegexp != nil && assert.Error(t, err) {
@@ -63,8 +64,9 @@ func TestResolveDevWorkspacePluginRegistry(t *testing.T) {
 				Errors:                tt.Input.Errors,
 			}
 			testResolverTools := ResolverTools{
-				Context:    context.Background(),
-				HttpClient: testHttpGetter,
+				Context:            context.Background(),
+				HttpClient:         testHttpGetter,
+				DefaultRegistryURL: "https://default-registry.com",
 			}
 			outputWorkspace, err := ResolveDevWorkspace(&tt.Input.Workspace, testResolverTools)
 			if tt.Output.ErrRegexp != nil && assert.Error(t, err) {
@@ -113,6 +115,76 @@ func TestResolveDevWorkspacePluginURI(t *testing.T) {
 
 func TestResolveDevWorkspaceParents(t *testing.T) {
 	tests := testutil.LoadAllTestsOrPanic(t, "testdata/parent")
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			// sanity check: input defines components
+			assert.True(t, len(tt.Input.Workspace.Components) > 0, "Test case defines workspace with no components")
+			testHttpGetter := &testutil.FakeHTTPGetter{
+				DevfileResources:      tt.Input.DevfileResources,
+				DevWorkspaceResources: tt.Input.DevWorkspaceResources,
+				Errors:                tt.Input.Errors,
+			}
+			testK8sClient := &testutil.FakeK8sClient{
+				DevWorkspaceResources: tt.Input.DevWorkspaceResources,
+				Errors:                tt.Input.Errors,
+			}
+			testResolverTools := ResolverTools{
+				Context:            context.Background(),
+				K8sClient:          testK8sClient,
+				HttpClient:         testHttpGetter,
+				DefaultNamespace:   "default-namespace",
+				DefaultRegistryURL: "default-registry-url",
+			}
+			outputWorkspace, err := ResolveDevWorkspace(&tt.Input.Workspace, testResolverTools)
+			if tt.Output.ErrRegexp != nil && assert.Error(t, err) {
+				assert.Regexp(t, *tt.Output.ErrRegexp, err.Error(), "Error message should match")
+			} else {
+				if !assert.NoError(t, err, "Should not return error") {
+					return
+				}
+				assert.Truef(t, cmp.Equal(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts),
+					"Workspace should match expected output:\n%s",
+					cmp.Diff(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts))
+			}
+		})
+	}
+}
+
+func TestResolveDevWorkspaceGeneralAPI(t *testing.T) {
+	tests := []testutil.TestCase{
+		testutil.LoadTestCaseOrPanic(t, "testdata/general/fail-nicely-when-no-http-client-provided_id.yaml"),
+		testutil.LoadTestCaseOrPanic(t, "testdata/general/fail-nicely-when-no-http-client-provided_uri.yaml"),
+		testutil.LoadTestCaseOrPanic(t, "testdata/general/fail-nicely-when-no-k8s-client-provided.yaml"),
+	}
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			// sanity check: input defines components
+			assert.True(t, len(tt.Input.Workspace.Components) > 0, "Test case defines workspace with no components")
+			testResolverTools := ResolverTools{
+				Context:    context.Background(),
+				K8sClient:  nil,
+				HttpClient: nil,
+			}
+			outputWorkspace, err := ResolveDevWorkspace(&tt.Input.Workspace, testResolverTools)
+			if tt.Output.ErrRegexp != nil && assert.Error(t, err) {
+				assert.Regexp(t, *tt.Output.ErrRegexp, err.Error(), "Error message should match")
+			} else {
+				if !assert.NoError(t, err, "Should not return error") {
+					return
+				}
+				assert.Truef(t, cmp.Equal(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts),
+					"Workspace should match expected output:\n%s",
+					cmp.Diff(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts))
+			}
+		})
+	}
+}
+
+func TestResolveDevWorkspaceMissingDefaults(t *testing.T) {
+	tests := []testutil.TestCase{
+		testutil.LoadTestCaseOrPanic(t, "testdata/general/fail-nicely-when-no-registry-url.yaml"),
+		testutil.LoadTestCaseOrPanic(t, "testdata/general/fail-nicely-when-no-namespace.yaml"),
+	}
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			// sanity check: input defines components
