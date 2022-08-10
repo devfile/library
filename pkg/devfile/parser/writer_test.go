624bd63107bdec34d425b5c8d12c2e19181c54d8
Stephanie <yangcao@redhat.com>
2022-08-10 17:09:30 -0400

    add writer tests
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- pkg/devfile/parser/writer_test.go
+++ pkg/devfile/parser/writer_test.go
@@ -1,8 +1,10 @@
 package parser
 
 import (
+	"fmt"
+	"strings"
 	"testing"
-
+	apiAttributes "github.com/devfile/api/v2/pkg/attributes"
 	v1 "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
 	devfilepkg "github.com/devfile/api/v2/pkg/devfile"
 	devfileCtx "github.com/devfile/library/pkg/devfile/parser/context"
@@ -13,8 +15,10 @@ import (
 func TestWriteYamlDevfile(t *testing.T) {
 
 	var (
-		schemaVersion = "2.0.0"
+		schemaVersion = "2.2.0"
 		testName      = "TestName"
+		uri = "./relative/path/deploy.yaml"
+		attributes = apiAttributes.Attributes{}.PutString(KubeComponentOriginalURIKey, uri)
 	)
 
 	t.Run("write yaml devfile", func(t *testing.T) {
@@ -33,6 +37,25 @@ func TestWriteYamlDevfile(t *testing.T) {
 							Name: testName,
 						},
 					},
+					DevWorkspaceTemplateSpec: v1.DevWorkspaceTemplateSpec{
+						DevWorkspaceTemplateSpecContent: v1.DevWorkspaceTemplateSpecContent{
+							Components: []v1.Component{
+								{
+									Name: "kubeComp",
+									Attributes: attributes,
+									ComponentUnion: v1.ComponentUnion{
+										Kubernetes: &v1.KubernetesComponent{
+											K8sLikeComponent: v1.K8sLikeComponent{
+												K8sLikeComponentLocation: v1.K8sLikeComponentLocation{
+													Inlined: "placeholder",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
 				},
 			},
 		}
@@ -45,6 +68,19 @@ func TestWriteYamlDevfile(t *testing.T) {
 
 		if _, err := fs.Stat(OutputDevfileYamlPath); err != nil {
 			t.Errorf("TestWriteYamlDevfile() unexpected error: '%v'", err)
+		}
+
+		data, err := fs.ReadFile(OutputDevfileYamlPath)
+		if err != nil {
+			t.Errorf("TestWriteYamlDevfile() unexpected error: '%v'", err)
+		} else {
+			content := string(data)
+			if strings.Contains(content, "inlined") || strings.Contains(content, KubeComponentOriginalURIKey) {
+				t.Errorf("TestWriteYamlDevfile() failed: kubernetes component should not contain inlined or %s", KubeComponentOriginalURIKey)
+			}
+			if !strings.Contains(content, fmt.Sprintf("uri: %s",uri)) {
+				t.Errorf("TestWriteYamlDevfile() failed: kubernetes component does not contain uri")
+			}
 		}
 	})
 }
