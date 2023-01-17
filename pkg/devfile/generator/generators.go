9245925dc46883164d62ef641fef8798f1780d1a
Parthvi Vala <pvala@redhat.com>
2023-01-17 19:35:26 +0530

    Remove error check for GetAttributes
    Signed-off-by: Parthvi Vala <pvala@redhat.com>

--- pkg/devfile/generator/generators.go
+++ pkg/devfile/generator/generators.go
@@ -191,11 +191,9 @@ func GetDeployment(devfileObj parser.DevfileObj, deployParams DeploymentParams) 
 	var globalAttributes attributes.Attributes
 	// attributes is not supported in versions less than 2.1.0, so we skip it
 	if devfileObj.Data.GetSchemaVersion() > string(data.APISchemaVersion200) {
-		var err error
-		globalAttributes, err = devfileObj.Data.GetAttributes()
-		if err != nil {
-			return nil, err
-		}
+		// the only time GetAttributes will return an error is if DevfileSchemaVersion is 2.0.0, a case we've already covered;
+		// so we'll skip checking for error here
+		globalAttributes, _ = devfileObj.Data.GetAttributes()
 	}
 	components, err := devfileObj.Data.GetDevfileContainerComponents(common.DevfileOptions{})
 	if err != nil {
